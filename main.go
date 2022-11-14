c131f9d4971da72a8fc11d859a198362955953b5
Ty <justfacey@gmail.com>
2022-11-14 06:03:34 -0800

    made createSnippet handler accept POST request only.

--- main.go
+++ main.go
@@ -5,14 +5,42 @@ import (
 	"net/http"
 )
 
+// Define a health check handler
 func home(w http.ResponseWriter, r *http.Request) {
+	// Checking if the URL patyh exactly matches "/"
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello for Snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	// Check if request is POST or not
+	if r.Method != http.MethodPost {
+		// Add 'Allow:POST' header to response
+		w.Header().Set("Allow", http.MethodPost)
+
+		// Send a 405 error if request is not POST
+		http.Error(w, "Method Not Allowed", 405)
+	}
+	w.Write([]byte("Create a new snippet"))
+}
+
 func main() {
+
+	// Spin up a servermux and registry home func as a handler for the "/" URL pattern
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Spin up a web server by passing in TCP network and servermux
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
