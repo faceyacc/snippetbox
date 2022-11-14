4605f13cbc1cb2254fc0392589afc713db23b202
Ty <justfacey@gmail.com>
2022-11-14 06:30:00 -0800

    sperated handlers and routing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,47 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-// Define a health check handler
-func home(w http.ResponseWriter, r *http.Request) {
-	// Checking if the URL patyh exactly matches "/"
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello for Snippetbox"))
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-
-	// Check if request is POST or not
-	if r.Method != http.MethodPost {
-		// Add 'Allow:POST' header to response
-		w.Header().Set("Allow", http.MethodPost)
-
-		// Send a 405 error if request is not POST
-		http.Error(w, "Method Not Allowed", 405)
-	}
-	w.Write([]byte("Create a new snippet"))
-}
 
 func main() {
 
