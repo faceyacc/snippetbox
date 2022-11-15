f46ded9b90a65840c94430f2153a2ebc64c2ca8d
Ty <justfacey@gmail.com>
2022-11-14 17:12:54 -0800

    added css and js to serve frontend

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,7 +17,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Use template.ParseFiles() to read template file into a template set
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,11 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create file server to serve out files from static directory
+	filerServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", filerServer))
+
 	// Spin up a web server by passing in TCP network and servermux
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
